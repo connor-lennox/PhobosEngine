1222d41ec343ad914bdeea9415020a4d9edd803e
Connor Lennox <clennox99@gmail.com>
2021-07-19 19:58:01 -0400

    More transform tests

--- PhobosEngine.Tests/Tests/Core/TransformTests.cs
+++ PhobosEngine.Tests/Tests/Core/TransformTests.cs
@@ -11,6 +11,14 @@ namespace PhobosEngine.Tests.Core
         private GameEntity entity;
         private Transform transform;
 
+        public Transform CreateChildTransform()
+        {
+            GameEntity childEntity = new GameEntity();
+            Transform childTransform = childEntity.Transform;
+            childTransform.Parent = transform;
+            return childTransform;
+        }
+
         [SetUp]
         public void Setup()
         {
@@ -45,7 +53,7 @@ namespace PhobosEngine.Tests.Core
         [Test]
         public void RotatedTransform_Up_Right_HaveCorrectValues()
         {
-            transform.Rotation = 90;
+            transform.Rotation = MathF.PI / 2;
             Assert.LessOrEqual(MathF.Abs(transform.Right.Y - 1), float.Epsilon);
             Assert.LessOrEqual(MathF.Abs(transform.Up.X + 1), float.Epsilon);
         }
@@ -59,13 +67,113 @@ namespace PhobosEngine.Tests.Core
             Assert.LessOrEqual(MathF.Abs(transform.Rotation), float.Epsilon);
 
             transform.PointTowards(Vector2.UnitY);
-            Assert.LessOrEqual(MathF.Abs(transform.Rotation - 90), float.Epsilon);
+            Assert.LessOrEqual(MathF.Abs(transform.Rotation - (MathF.PI / 2)), float.Epsilon);
 
             transform.PointTowards(new Vector2(1, 1));
-            Assert.LessOrEqual(MathF.Abs(transform.Rotation - 45), float.Epsilon);
+            Assert.LessOrEqual(MathF.Abs(transform.Rotation - (MathF.PI / 4)), float.Epsilon);
 
             transform.PointTowards(new Vector2(-1, 1));
-            Assert.LessOrEqual(MathF.Abs(transform.Rotation - 135), float.Epsilon);
+            Assert.LessOrEqual(MathF.Abs(transform.Rotation - (3 * MathF.PI / 4)), float.Epsilon);
+        }
+
+        [Test]
+        public void ChildTransform_SetLocalPosition_CorrectWorldPosition()
+        {
+            transform.Position = new Vector2(0, 0);
+
+            Transform childTransform = CreateChildTransform();
+            childTransform.LocalPosition = new Vector2(1, 1);
+
+            Assert.AreEqual(childTransform.Position, new Vector2(1, 1));
+        }
+
+        [Test]
+        public void ParentTransformMoves_ChildFollows()
+        {
+            transform.Position = new Vector2(0, 0);
+
+            GameEntity childEntity = new GameEntity();
+            Transform childTransform = CreateChildTransform();
+            
+            Assert.AreEqual(childTransform.Position, Vector2.Zero);
+
+            transform.Position = new Vector2(1, 1);
+
+            Assert.AreEqual(childTransform.Position, new Vector2(1, 1));
+            Assert.AreEqual(childTransform.LocalPosition, Vector2.Zero);
+        }
+
+        [Test]
+        public void ParentAndChildMove_PositionCorrect()
+        {
+            Transform childTransform = CreateChildTransform();
+
+            transform.Position = new Vector2(2, 2);
+            childTransform.LocalPosition = new Vector2(1, 1);
+
+            Assert.AreEqual(childTransform.Position, new Vector2(3, 3));
+        }
+
+        [Test]
+        public void ChildTransform_PositionSet_CorrectLocalPosition()
+        {
+            Transform childTransform = CreateChildTransform();
+
+            transform.Position = new Vector2(1, 1);
+            childTransform.Position = Vector2.Zero;
+            
+            Assert.AreEqual(childTransform.LocalPosition, -Vector2.One);
+        }
+
+        [Test]
+        public void RootTransform_RotationTests()
+        {
+            transform.Rotation = 1;
+            Assert.AreEqual(transform.LocalRotation, 1);
+
+            transform.RotationDegrees = 180;
+            Assert.AreEqual(transform.LocalRotation, MathF.PI);
+
+            transform.LocalRotation = MathF.PI / 2;
+            Assert.AreEqual(transform.RotationDegrees, 90);
+        }
+
+        [Test]
+        public void RootTransform_ScaleTests()
+        {
+            transform.Scale = 2 * Vector2.One;
+            Assert.AreEqual(transform.LocalScale, 2 * Vector2.One);
+
+            transform.LocalScale = Vector2.One;
+            Assert.AreEqual(transform.Scale, Vector2.One);
+        }
+
+        [Test]
+        public void ChildTransform_CorrectRotation()
+        {
+            Transform childTransform = CreateChildTransform();
+
+            transform.Rotation = 1;
+            Assert.AreEqual(childTransform.Rotation, 1);
+            Assert.AreEqual(childTransform.LocalRotation, 0);
+
+            childTransform.Rotation = 2;
+            Assert.AreEqual(childTransform.Rotation, 2);
+            Assert.AreEqual(childTransform.LocalRotation, 1);
+        }
+
+        [Test]
+        public void ChildTransform_CorrectScale()
+        {
+            Transform childTransform = CreateChildTransform();
+
+            transform.Scale = 2 * Vector2.One;
+            Assert.AreEqual(childTransform.Scale, 2 * Vector2.One);
+            Assert.AreEqual(childTransform.LocalScale, Vector2.One);
+
+            childTransform.Scale = Vector2.One;
+            Assert.AreEqual(childTransform.Scale, Vector2.One);
+            Assert.AreEqual(childTransform.LocalScale, new Vector2(0.5f, 0.5f));
         }
     }
 }
