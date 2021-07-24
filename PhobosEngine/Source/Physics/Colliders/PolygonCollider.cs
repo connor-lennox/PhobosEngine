344ed2e928d84573df32a130e9726dcdbb209bfa
Connor Lennox <clennox99@gmail.com>
2021-07-24 10:47:26 -0400

    fix error with < 3 points

--- PhobosEngine/Source/Physics/Colliders/PolygonCollider.cs
+++ PhobosEngine/Source/Physics/Colliders/PolygonCollider.cs
@@ -6,7 +6,7 @@ namespace PhobosEngine
 {
     public class PolygonCollider : Collider
     {
-        protected Vector2[] points;
+        protected Vector2[] points = new Vector2[0];
         public Vector2[] Points {
             get => points;
             set {
@@ -35,27 +35,27 @@ namespace PhobosEngine
         protected override void RecalculateBounds()
         {
             CalculateEffectivePoints();
-            if(points.Length < 1)
+            if(points.Length < 3)
             {
                 Bounds = RectangleF.Empty;
+            } else {
+                float minX = effectivePoints[0].X;
+                float maxX = effectivePoints[0].X;
+                float minY = effectivePoints[0].Y;
+                float maxY = effectivePoints[0].Y;
+
+                for(int i = 1; i < effectivePoints.Length; i++)
+                {
+                    float x = effectivePoints[i].X;
+                    float y = effectivePoints[i].Y;
+                    minX = MathF.Min(minX, x);
+                    maxX = MathF.Max(maxX, x);
+                    minY = MathF.Min(minY, y);
+                    maxY = MathF.Max(maxY, y);
+                }
+
+                Bounds = new RectangleF(minX, minY, (maxX - minX), (maxY - minY));
             }
-
-            float minX = effectivePoints[0].X;
-            float maxX = effectivePoints[0].X;
-            float minY = effectivePoints[0].Y;
-            float maxY = effectivePoints[0].Y;
-
-            for(int i = 1; i < effectivePoints.Length; i++)
-            {
-                float x = effectivePoints[i].X;
-                float y = effectivePoints[i].Y;
-                minX = MathF.Min(minX, x);
-                maxX = MathF.Max(maxX, x);
-                minY = MathF.Min(minY, y);
-                maxY = MathF.Max(maxY, y);
-            }
-
-            Bounds = new RectangleF(minX, minY, (maxX - minX), (maxY - minY));
         }
     }
 }
