413c2b02027bfa16728eb0190e854162fc551aab
Connor Lennox <clennox99@gmail.com>
2021-07-24 15:40:11 -0400

    aabb collider collisions

---
+++ PhobosEngine/Source/Physics/CollisionHandling/AABBCollisions.cs
@@ -0,0 +1,16 @@
+namespace PhobosEngine
+{
+    public static class AABBCollisions
+    {
+        public static bool AABBToAABB(AABBCollider b1, AABBCollider b2, out CollisionResult result)
+        {
+            result = new CollisionResult();
+
+            bool collided = b1.Bounds.Intersects(b2.Bounds);
+
+            // TODO: can some of the result properties be filled in?
+
+            return collided;
+        }
+    }
+}
