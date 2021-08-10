e0e0c34471cdf6216fcb743cbd027321052238ef
Connor Lennox <clennox99@gmail.com>
2021-08-10 17:35:15 -0400

    update debug scene

--- PhobosEngine/PhobosEngine.cs
+++ PhobosEngine/PhobosEngine.cs
@@ -15,6 +15,7 @@ namespace PhobosEngine
         private Transform testTransform;
 
         private Transform[] debugCorners;
+        private BoxCollider boxToDebug;
 
         public PhobosEngine()
         {
@@ -61,22 +62,44 @@ namespace PhobosEngine
             tracker.OnCollisionExit += ((CollisionResult result) => testEntity1.GetComponent<SpriteRenderer>().tintColor = Color.White);
 
             GameEntity testEntity2 = new GameEntity();
-            testEntity2.Transform.Position = new Vector2(0, 0);
+            testEntity2.Transform.Position = new Vector2(-40, 0);
             testEntity2.AddComponent(new SpriteRenderer()).sprite = testPixelTexture;
             testEntity2.GetComponent<SpriteRenderer>().tintColor = Color.Black;
             BoxCollider c2 = testEntity2.AddComponent<BoxCollider>();
             c2.Size = new Vector2(20, 20);
 
+            GameEntity testEntity3 = new GameEntity();
+            testEntity3.Transform.Position = new Vector2(30, -10);
+            testEntity3.AddComponent(new SpriteRenderer()).sprite = testPixelTexture;
+            testEntity3.GetComponent<SpriteRenderer>().tintColor = Color.Black;
+            BoxCollider c3 = testEntity3.AddComponent<BoxCollider>();
+            c3.Size = new Vector2(20, 20);
+
             c1.Register();
             c2.Register();
+            c3.Register();
 
             GameEntity camEntity = new GameEntity();
             camEntity.AddComponent(new Camera());
             camEntity.GetComponent<Camera>().Bounds = GraphicsDevice.Viewport.Bounds;
+            testScene.AddEntity(testEntity3);
             testScene.AddEntity(testEntity2);
             testScene.AddEntity(testEntity1);
             testScene.AddEntity(camEntity);
             testScene.MainCamera = camEntity.GetComponent<Camera>();
+
+            GameEntity test2 = new GameEntity();
+            test2.Transform.Parent = testTransform;
+            test2.AddComponent<SpriteRenderer>().sprite = testPixelTexture;
+            test2.GetComponent<SpriteRenderer>().tintColor = Color.Olive;
+            test2.Transform.LocalPosition = new Vector2(50, 0);
+            testScene.AddEntity(test2);
+            boxToDebug = test2.AddComponent<BoxCollider>();
+            boxToDebug.Size = new Vector2(20, 20);
+            CollisionTracker tracker2 = test2.AddComponent<CollisionTracker>();
+            tracker2.OnCollisionEnter += ((CollisionResult res) => test2.GetComponent<SpriteRenderer>().tintColor = Color.DarkViolet);
+            tracker2.OnCollisionExit += ((CollisionResult res) => test2.GetComponent<SpriteRenderer>().tintColor = Color.Olive);
+            boxToDebug.Register();
 
             debugCorners = new Transform[4];
             for(int i = 0; i < 4; i++)
@@ -84,16 +107,9 @@ namespace PhobosEngine
                 debugCorners[i] = new GameEntity().Transform;
                 debugCorners[i].AddComponent<SpriteRenderer>().sprite = smallPixelTexture;
                 debugCorners[i].GetComponent<SpriteRenderer>().tintColor = Color.Lime;
-                // debugCorners[i].Scale = new Vector2(0.2f, 0.2f);
                 testScene.AddEntity(debugCorners[i].Entity);
             }
 
-            // GameEntity test2 = new GameEntity();
-            // test2.Transform.Parent = testTransform;
-            // test2.AddComponent<SpriteRenderer>().sprite = testPixelTexture;
-            // test2.GetComponent<SpriteRenderer>().tintColor = Color.Olive;
-            // test2.Transform.LocalPosition = new Vector2(50, 0);
-            // testScene.AddEntity(test2);
         }
 
         protected override void Update(GameTime gameTime)
@@ -102,18 +118,17 @@ namespace PhobosEngine
                 Exit();
 
             testScene.Update();
-            float x = MathF.Cos((float)gameTime.TotalGameTime.TotalSeconds / 2);
-            float y = MathF.Sin((float)gameTime.TotalGameTime.TotalSeconds / 2);
-            testTransform.Position = new Vector2(x, y) * 100;
+            float x = MathF.Cos((float)gameTime.TotalGameTime.TotalSeconds);
+            float y = MathF.Sin((float)gameTime.TotalGameTime.TotalSeconds);
+            testTransform.Position = new Vector2(x, y) * 40;
             // testTransform.Position = new Vector2(100, 100);
-            testTransform.PointTowards(Vector2.Zero);
-            // testTransform.Rotation = x;
+            // testTransform.PointTowards(Vector2.Zero);
+            testTransform.Rotation = x;
             // testTransform.Scale = new Vector2(MathF.Abs(x)+.25f, MathF.Abs(y)+.25f);
 
-            BoxCollider col = testTransform.GetComponent<BoxCollider>();
             for(int i = 0; i < 4; i++)
             {
-                debugCorners[i].Position = col.EffectivePoints[i];
+                debugCorners[i].Position = boxToDebug.EffectivePoints[i];
             }
 
             base.Update(gameTime);
